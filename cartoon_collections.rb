ce9b72b909bfecb4a600dd95ee09fb69eba028e8
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 03:54:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(array)
   array.map{ |word| word.capitalize + "!"} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each{ |words| words.size > 4? true:false }
 end
 
 def find_the_cheese# code an argument here
