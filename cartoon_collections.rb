aa5f95a1b9f2a3338d5e126836ed190146435ae8
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 04:28:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,7 @@ def long_planeteer_calls(array)
   return false 
 end 
 
+#find the cheese string in the cheese_types array amongst the array of random strings
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
