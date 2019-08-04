ef4d25d12fd8db54688b6c4f8b86a923a026c332
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 03:59:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each{ |words| words.any? > 4? true:false }
+  array.each{ |words| words.any? > 4? }
 end
 
 def find_the_cheese# code an argument here
