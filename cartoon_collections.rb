9a21198bbf358dc4c7538bd8dd88de5b89b40cdc
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 03:35:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  [11,22,31,224,44].each_with_index { |val,index| puts "index: #{index} for #{val}"
+def roll_call_dwarves(array)
+  array.each_with_index { |val,index| puts "#{index}. #{val}"}
 end
 
 def summon_captain_planet# code an argument here
